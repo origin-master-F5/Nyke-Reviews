66e6d3d50eaf5adc5a216f2c0627b7d9edbe35f9
Darth Varg <cbvargas123@gmail.com>
2020-04-07 18:04:00 -0700

    added default values to psql schema

--- database-postgresql/schema.sql
+++ database-postgresql/schema.sql
@@ -1,22 +1,4 @@
-CREATE TABLE reviews (
-  _id BIGSERIAL NOT NULL PRIMARY KEY,
-  header VARCHAR(50),
-  comment TEXT,
-  star INT,
-  size INT,
-  comfort INT,
-  durability INT,
-  dateWritten TEXT,
-  username VARCHAR(50),
-  location VARCHAR(50),
-  avgRunDistance VARCHAR(20),
-  terrain VARCHAR(20),
-  flagged INT,
-  upvotes INT,
-  downvotes INT,
-  verified BOOLEAN,
-  image VARCHAR(150)
-);
+
 
 CREATE TABLE products (
   _id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -24,5 +6,29 @@ CREATE TABLE products (
   productId INT NOT NULL,
   price MONEY,
   discountPrice MONEY,
-  productImage TEXT
+  productImage TEXT,
+  UNIQUE(productId)
 );
+
+CREATE TABLE reviews (
+  _id BIGSERIAL NOT NULL PRIMARY KEY,
+  header VARCHAR(100),
+  comment TEXT,
+  star INT DEFAULT 3,
+  size INT DEFAULT 1,
+  comfort INT DEFAULT 1,
+  durability INT DEFAULT 1,
+  dateWritten TEXT ,
+  username VARCHAR(50),
+  location VARCHAR(150),
+  avgRunDistance VARCHAR(20),
+  terrain VARCHAR(20),
+  flagged INT DEFAULT 0,
+  upvotes INT DEFAULT 0,
+  downvotes INT DEFAULT 0,
+  verified BOOLEAN DEFAULT false,
+  image VARCHAR(150),
+  productId BIGINT NOT NULL REFERENCES products (_id),
+  UNIQUE(productId)
+);
+
